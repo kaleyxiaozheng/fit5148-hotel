9621ee6cd3400cc204ae14fe8dc56e75d1dde193
TT <nguyenthanhthao747@gmail.com>
2017-03-24 15:38:01 +1100

    update sql

--- HotelAppFIT5148/sql_statement/fit5148a_thao.sql
+++ HotelAppFIT5148/sql_statement/fit5148a_thao.sql
@@ -9,9 +9,6 @@ exception
 end;
 /
 CREATE TABLE  hotel (
-  hotel_id number(6),
-  name varchar(50),
-  hotel_type varchar(20),
   hotel_id number(6) not null,
   hotel_name varchar(50),
   hotel_type number(2),
@@ -39,9 +36,17 @@ CREATE INDEX hotel_type_index ON hotel(hotel_type);
 --DROP SEQUENCE hotel_seq; 
 
 INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
-   VALUES (hotel_seq.nextval, 'Diamond Dust', 5, 1991, 'USA', 'New York', '13 Rose Street', '01254747747', 'diamonddust@gmail.com' );
+   VALUES (null, 'Diamond Dust', 5, 1991, 'USA', 'New York', '13 Rose Street', '01254747747', 'diamonddust@gmail.com' );
 INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
- VALUES (null, 'Diamond Dust 2', 5, 1991, 'USA', 'New York', '13 Rose Street', '01254747747', 'diamonddust@gmail.com' );
+ VALUES (null, 'Heaven', 4, 1890, 'Vietnam', 'HCM', '13 Amazing rose', '01254747747', 'Heaven@gmail.com' );
+INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
+   VALUES (null, 'Happinese', 3, 1991, 'USA', 'New York', '13 North Road', '01254747747', 'happinese@gmail.com' );
+INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
+ VALUES (null, 'Love Paradise', 2, 1990, 'China', 'Bejjing', '209 Rose Street', '01254734333', 'loveparadise@gmail.com' );
+INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
+   VALUES (null, 'Big Hero', 5, 2007, 'Australia', 'Melbourne', '13 Flinder Street', '0125474343', 'bighero@gmail.com' );
+INSERT INTO hotel (hotel_id, hotel_name, hotel_type, construction_year, country, city, address, contact_number, email)
+ VALUES (null, 'Cannal', 1, 2008, 'Indonesia', 'Bali', '13 No way street', '01254574232', 'cannal@gmail.com' );
 
 commit;
 
