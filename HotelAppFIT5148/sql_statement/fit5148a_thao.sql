75c5f0a191b0606aa040c2531abbb18b2eaca96e
TT <nguyenthanhthao747@gmail.com>
2017-03-24 12:25:28 +1100

    Create project and sql statement

---
+++ HotelAppFIT5148/sql_statement/fit5148a_thao.sql
@@ -0,0 +1,23 @@
+
+begin 
+  execute immediate 'drop table hotel';
+exception
+  when others then
+    if sqlcode != -942 then
+      raise;
+    end if;
+end;
+/
+CREATE TABLE  hotel (
+  hotel_id number(6),
+  name varchar(50),
+  hotel_type varchar(20),
+  construction_year number(6),
+  country varchar(20),
+  city varchar(20),
+  address varchar(50),
+  contact_number varchar(20),
+  email varchar(50),
+  CONSTRAINT hotel_pk PRIMARY KEY (hotel_id)
+);
+
