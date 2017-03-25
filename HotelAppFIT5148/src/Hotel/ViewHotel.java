41e1e5aaeefa995012a4994f1c42c004692bd3f6
TT <nguyenthanhthao747@gmail.com>
2017-03-26 02:03:41 +1100

    Add search function

--- HotelAppFIT5148/src/Hotel/ViewHotel.java
+++ HotelAppFIT5148/src/Hotel/ViewHotel.java
@@ -9,13 +9,15 @@ import hotelappfit5148.DBConnection;
 import javax.swing.table.DefaultTableModel;
 import oracle.jdbc.OracleDriver;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author thaonguyen
  */
 public class ViewHotel extends javax.swing.JFrame {
-
+    
     Object columnHeaders[] = {"HOTEL_ID", "HOTEL_NAME", "HOTEL_TYPE", "CONSTUCTION_YEAR", "COUNTRY", "CITY", "ADDRESS", "CONTACT_NUMBER", "EMAIL"};
     Object data[][] = {{}};
     DefaultTableModel dtm = new DefaultTableModel(data, columnHeaders);
@@ -28,12 +30,14 @@ public class ViewHotel extends javax.swing.JFrame {
     public ViewHotel() {
         initComponents();
         conn = DBConnection.getDBConnection("FIT5148A");
-
+        
         try {
             DriverManager.registerDriver(new OracleDriver());
             stmt = conn.createStatement();
-            ResultSet rset = stmt.executeQuery("select * from hotel");
-
+            ResultSet rset = stmt.executeQuery("select hotel_id, hotel_name, hotel_type, "
+                    + "construction_year, country, city, address, "
+                    + "contact_number, email from hotel");
+            
             ResultSetMetaData mdata = rset.getMetaData();
             int numberOfColumns = mdata.getColumnCount();
             while (rset.next()) {
@@ -43,7 +47,7 @@ public class ViewHotel extends javax.swing.JFrame {
                 }
                 dtm.addRow(rowData);
             }
-
+            
         } catch (SQLException f) {
             System.out.println(f.getMessage());
         }
@@ -61,6 +65,9 @@ public class ViewHotel extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -69,24 +76,52 @@ public class ViewHotel extends javax.swing.JFrame {
 
         jLabel1.setText("View Hotel");
 
+        jLabel2.setText("Hotel Type");
+
+        List<String> typeListCombox = new ArrayList<String>();
+        typeListCombox.add("None");
+        typeListCombox.addAll(getHotelType());
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(typeListCombox.toArray()));
+
+        jButton1.setText("Search");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 808, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1))
+                        .addGap(18, 18, 18)
+                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 237, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29))
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(50, 50, 50)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(74, Short.MAX_VALUE))
         );
 
         jLabel1.getAccessibleContext().setAccessibleName("viewHotelLabel");
@@ -94,6 +129,57 @@ public class ViewHotel extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        dtm.setRowCount(0);
+        
+        try {
+            // TODO add your handling code here:
+
+            String selectedHotelType = String.valueOf(jComboBox2.getSelectedItem());
+            StringBuffer sbSQL = new StringBuffer("select hotel_id, hotel_name, hotel_type, "
+                    + "construction_year, country, city, address, "
+                    + "contact_number, email from hotel");
+            if (!"None".equals(selectedHotelType)) {
+                sbSQL.append(" where hotel_type = '" + selectedHotelType + "'");
+            }
+            
+            ResultSet rset = DBConnection.selectRecords("FIT5148A", sbSQL.toString());
+            ResultSetMetaData mdata = rset.getMetaData();
+            int numberOfColumns = mdata.getColumnCount();
+            while (rset.next()) {
+                Object[] rowData = new Object[numberOfColumns];
+                for (int i = 0; i < rowData.length; i++) {
+                    rowData[i] = rset.getObject(i + 1);
+                }
+                dtm.addRow(rowData);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+    
+    public List<String> getHotelType() {
+        String getHotelType = "select distinct hotel_type from hotel order by hotel_type";
+        
+        try {
+            ResultSet rset = DBConnection.selectRecords("FIT5148A", getHotelType);
+            
+            List<String> hotelTypeList = new ArrayList<String>();
+            while (rset.next()) {
+                hotelTypeList.add(rset.getString(1));
+            }
+//            List<String> result = hotelTypeList.toArray(new String[hotelTypeList.size()]);
+            return hotelTypeList;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return null;
+    }
 
     /**
      * @param args the command line arguments
@@ -131,7 +217,10 @@ public class ViewHotel extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox jComboBox2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
