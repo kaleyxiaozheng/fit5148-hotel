7c247f11a288debf4b1945c5c2dafd6f418e8314
situsnow <situsnow@163.com>
2017-04-03 21:39:16 +1000

    Fix the error

--- HotelAppFIT5148/src/Room/RoomPanel.java
+++ HotelAppFIT5148/src/Room/RoomPanel.java
@@ -127,6 +127,7 @@ public class RoomPanel extends javax.swing.JPanel {
         deleteRoomButton = new javax.swing.JButton();
         HotelScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         facilityTextField1 = new javax.swing.JTextField();
 
@@ -144,7 +145,7 @@ public class RoomPanel extends javax.swing.JPanel {
             }
         });
 
-        updateRoomButton.setText("Update Room");
+        updateRoomButton.setText("Update");
         updateRoomButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 updateRoomButtonActionPerformed(evt);
@@ -161,6 +162,8 @@ public class RoomPanel extends javax.swing.JPanel {
         jTable1.setModel(dtm);
         HotelScrollPane1.setViewportView(jTable1);
 
+        jLabel1.setText("View Room");
+
         jLabel2.setText("Facility Name");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -169,22 +172,6 @@ public class RoomPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(HotelScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(facilityTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(searchButton)))
-                .addGap(11, 11, 11))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addComponent(newRoomButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(updateRoomButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(deleteRoomButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -208,14 +195,6 @@ public class RoomPanel extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(searchButton)
-                    .addComponent(facilityTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(HotelScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -230,7 +209,6 @@ public class RoomPanel extends javax.swing.JPanel {
                     .addComponent(newRoomButton)
                     .addComponent(updateRoomButton)
                     .addComponent(deleteRoomButton))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(24, 24, 24))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -367,6 +345,7 @@ public class RoomPanel extends javax.swing.JPanel {
     private javax.swing.JScrollPane HotelScrollPane1;
     private javax.swing.JButton deleteRoomButton;
     private javax.swing.JTextField facilityTextField1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JTable jTable1;
     private javax.swing.JButton newRoomButton;
